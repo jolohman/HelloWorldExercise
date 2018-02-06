852c4e4666cba4beae1da19f5f412d09070b11d6
Josh Lohman <jjosh9708@gmail.com>
2018-02-06 10:30:54 -0500

    Add project files.

---
+++ HelloWorldExercise.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelloWorldExercise.Web.Startup))]
+namespace HelloWorldExercise.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
